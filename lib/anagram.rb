359087c51b4c57bbe20ce721278c2e638b11ef97
yurubahmed <yurubiftin2002@gmail.com>
2023-02-12 11:44:07 -0500

    commit tree cd49854ac81133cf0c50653aa166d6ff86f7e35b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,9 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @name = word 
+    end
+    def match(array)
+        array.select {|item| item.split("").sort == @name.split("").sort}
+    end 
+end
